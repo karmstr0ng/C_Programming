c030bf092f907800ef4b10a47fd9bc4d22ed7239
karmstr0ng <kevinarmstr0ng@hotmail.com>
2019-01-15 18:13:36 -0800

    sizeof() example

---
+++ examples/size_of_function/exp1.c
@@ -0,0 +1,25 @@
+// sizeof() function examples
+#include <stdio.h>
+
+int main() {
+  char  c;
+  short s;
+  int   d;
+  float f;
+  
+
+  printf("size of char:  %d byte(s)\n", sizeof(c));
+  printf("size of short: %d byte(s)\n", sizeof(s));
+  printf("size of int:   %d byte(s)\n", sizeof(d));
+  printf("size of int:   %d byte(s)\n", sizeof(int));
+  printf("size of float: %d byte(s)\n", sizeof(f));
+  printf("size of float: %d byte(s)\n", sizeof(float));
+
+  return (0);
+}
+
+
+
+
+
+
